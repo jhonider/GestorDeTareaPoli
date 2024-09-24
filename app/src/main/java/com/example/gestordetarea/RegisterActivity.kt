59e932af6dd766211c9c0ed6c6021d27f0ac5a90
Laurahdez178 <lahernandez43@poligran.edu.co>
2024-09-23 23:05:01 -0500

    Pantalla para registro
    Cree la pantalla para generar el registro, anclada al boton de registrarse del la pagina de inicio

--- app/src/main/java/com/example/gestordetarea/RegisterActivity.kt
+++ app/src/main/java/com/example/gestordetarea/RegisterActivity.kt
@@ -1,28 +1,20 @@
 package com.example.gestordetarea
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val openButton: TextView = findViewById(R.id.registerButton)
-        openButton.setOnClickListener {
-            // Crea un Intent para abrir la nueva actividad
-            val intent = Intent(this, RegisterActivity::class.java)
-            startActivity(intent)
         }
     }
 }
